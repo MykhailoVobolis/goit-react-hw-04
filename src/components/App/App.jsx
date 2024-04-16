9f1b8ad2dfbb35d4650496f482494b501a69cd91
MykhailoVobolis <mvobolis@gmail.com>
2024-04-16 21:36:46 +0300

    new loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -38,6 +38,7 @@ export default function App() {
 
   const nextPage = () => {
     setLoaderBtn(false);
+    setLoading(true);
     setPage(page + 1);
   };
 
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,14 @@
-import css from "./Loader.module.css";
-import RiseLoader from "react-spinners/RiseLoader";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Loader({ loading }) {
   return (
-    <div className={css.loader}>
-      <RiseLoader color={"#4830f0"} loading={loading} size={10} />
-    </div>
+    <ColorRing
+      visible={loading}
+      height="70"
+      width="70"
+      ariaLabel="color-ring-loading"
+      wrapperClass="color-ring-wrapper"
+      colors={["#de367f", "#4830f0", "#de367f", "#4830f0", "#de367f"]}
+    />
   );
 }
