d2dea107429bb5623f7e8b5850267af1c1739421
MykhailoVobolis <mvobolis@gmail.com>
2024-04-17 11:23:24 +0300

    small fix loader animation

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,15 @@
 import { ColorRing } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 export default function Loader({ loading }) {
   return (
     <ColorRing
       visible={loading}
-      height="70"
-      width="70"
-      ariaLabel="color-ring-loading"
-      wrapperClass="color-ring-wrapper"
-      colors={["#de367f", "#4830f0", "#de367f", "#4830f0", "#de367f"]}
+      height="80"
+      width="80"
+      ariaLabel="color-ring-wrapper"
+      wrapperClass={css.colorRingLoading}
+      colors={["#4830f0", "#de367f", "#4830f0", "#de367f", "#4830f0"]}
     />
   );
 }
